bf10b6826101320e15cccd0a46f3b435abb153e2
Przemko Robakowski <przemko@przemko-robakowski.pl>
2023-04-11 13:39:27 +0200

    improved game

--- src/main/java/pl/robakowski/game/Game.java
+++ src/main/java/pl/robakowski/game/Game.java
@@ -3,43 +3,31 @@ package pl.robakowski.game;
 import com.dslplatform.json.CompiledJson;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 
 @CompiledJson
 public record Game(int groupCount, List<Clan> clans) {
 
-    private static final ThreadLocal<ArrayList<Group>> group = ThreadLocal.withInitial(() -> new ArrayList<>(20000));
-
     public List<Group> getOrder() {
         var groups = new LinkedList<Group>();
-        var allGroups = group.get();
-        allGroups.clear();
         clans.sort(null);
-        for (Clan clan : clans) {
-            putInGroup(groups, allGroups, clan);
-        }
-
-        return allGroups;
-    }
-
-    private void putInGroup(LinkedList<Group> groups, ArrayList<Group> allGroups, Clan clan) {
-        var it = groups.iterator();
-        while (it.hasNext()) {
-            Group next = it.next();
-            if (next.getCapacity() >= clan.numberOfPlayers()) {
-                next.add(clan);
-                if (next.getCapacity() == 0) {
+        while (!clans.isEmpty()) {
+            int available = groupCount;
+            Group e = new Group(groupCount);
+            groups.add(e);
+            Iterator<Clan> it = clans.iterator();
+            while (it.hasNext() && available > 0) {
+                Clan next = it.next();
+                if (next.numberOfPlayers() <= available) {
                     it.remove();
+                    e.add(next);
+                    available -= next.numberOfPlayers();
                 }
-                return;
             }
         }
-        Group group = new Group(groupCount);
-        allGroups.add(group);
-        group.add(clan);
-        if (group.getCapacity() > 0) {
-            groups.add(group);
-        }
+
+        return groups;
     }
 }
