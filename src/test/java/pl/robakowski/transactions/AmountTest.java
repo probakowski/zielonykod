797f70b43d370c4e9acb9d2e67c04b5305c1d237
Przemko Robakowski <przemko@przemko-robakowski.pl>
2023-04-24 11:22:28 +0200

    accountnumber

--- src/test/java/pl/robakowski/transactions/AmountTest.java
+++ src/test/java/pl/robakowski/transactions/AmountTest.java
@@ -9,18 +9,7 @@ import java.util.Random;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class AmountTest {
-
-    private static final Random RANDOM = new Random();
-
-    @Test
-    public void amountReaderShouldWork() {
-        for (int i = 0; i < 100000; i++) {
-
-            JsonReader<?>reader = new JsonReader<>();
-            AmountConverter.JSON_READER.read(reader)
-        }
-    }
-
+    
     @Test
     public void checkDouble() {
         for (long i = 92233000000000L; i < 92233720368500L; i++) {
