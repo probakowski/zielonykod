3783baf706b743d637e89d46fe7691e44b862938
Przemko Robakowski <przemko@przemko-robakowski.pl>
2023-04-24 20:55:39 +0200

    cleanup

--- src/main/java/pl/robakowski/atms/AtmHandler.java
+++ src/main/java/pl/robakowski/atms/AtmHandler.java
@@ -13,13 +13,13 @@ import java.util.Objects;
 public class AtmHandler extends Handler {
 
     private static final JsonWriter.WriteObject<Atm> atmWriter = Objects.requireNonNull(json.tryFindWriter(Atm.class));
-    private static final int requestTypesCount = 4;
-    private static final int maxRegionNumber = 9999;
-    private static final int bucketsCount = requestTypesCount * (maxRegionNumber + 1);
+    private static final int REQUEST_TYPES_COUNT = 4;
+    private static final int MAX_REGION_NUMBER = 9999;
+    private static final int BUCKETS_COUNT = REQUEST_TYPES_COUNT * (MAX_REGION_NUMBER + 1);
 
     @Override
     protected void handle(InputStream is, JsonWriter writer) throws Exception {
-        List<Atm>[] buckets = new List[bucketsCount];
+        List<Atm>[] buckets = new List[BUCKETS_COUNT];
         Iterator<Request> requests = json.iterateOver(Request.class, is);
         int requestsCount = 0;
         if (requests == null) {
@@ -29,16 +29,16 @@ public class AtmHandler extends Handler {
         while (requests.hasNext()) {
             requestsCount++;
             Request request = requests.next();
-            int region = request.getRegion() * requestTypesCount + request.getRequestType().ordinal();
+            int region = request.getRegion() * REQUEST_TYPES_COUNT + request.getRequestType().ordinal();
             if (buckets[region] == null) {
                 buckets[region] = new ArrayList<>();
             }
             buckets[region].add(request);
         }
         List<Atm> atms = new ArrayList<>(requestsCount);
-        for (int i = 0; i < buckets.length; i += requestTypesCount) {
+        for (int i = 0; i < buckets.length; i += REQUEST_TYPES_COUNT) {
             BitSet visited = new BitSet();
-            for (int j = 0; j < requestTypesCount; j++) {
+            for (int j = 0; j < REQUEST_TYPES_COUNT; j++) {
                 if (buckets[i + j] != null) {
                     for (Atm atm : buckets[i + j]) {
                         if (visited.set(atm.getAtmId())) {
