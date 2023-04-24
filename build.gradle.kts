3503f83f30fa44e58f1912caf50adaf45bd9a55d
Przemko Robakowski <przemko@przemko-robakowski.pl>
2023-04-24 20:49:56 +0200

    build workflow

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,6 @@ plugins {
     id("java")
     id("application")
     id("com.github.spotbugs") version "5.0.14"
-//    id("com.github.johnrengelman.shadow") version "8.1.1"
 }
 
 group = "pl.robakowski"
@@ -20,6 +19,7 @@ java {
 tasks.test {
     minHeapSize = "2048m"
     maxHeapSize = "2048m"
+    useJUnitPlatform()
 }
 
 val integration: SourceSet by sourceSets.creating
@@ -32,7 +32,7 @@ with(integration) {
     compileClasspath += sourceSets.main.get().output
 }
 
-val e2eTestTask = tasks.register<Test>("e2eTest") {
+val e2eTask = tasks.register<Test>("e2e") {
     description = "Runs E2E tests."
     group = "verification"
     useJUnitPlatform()
@@ -43,14 +43,9 @@ val e2eTestTask = tasks.register<Test>("e2eTest") {
 
     shouldRunAfter(tasks.test)
 
-    minHeapSize = "2G"
-    maxHeapSize = "2G"
+    minHeapSize = "8G"
+    maxHeapSize = "8G"
 }
-
-tasks.check {
-    dependsOn(e2eTestTask)
-}
-
 
 spotbugs {
     excludeFilter.set(file("spotbugs.exclude.xml"))
@@ -77,8 +72,4 @@ dependencies {
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.2")
 
     spotbugsPlugins("com.h3xstream.findsecbugs:findsecbugs-plugin:1.12.0")
-}
-
-tasks.getByName<Test>("test") {
-    useJUnitPlatform()
 }
